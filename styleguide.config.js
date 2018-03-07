f2ca4ca40bdaccd8b6e34d3066eb2e55221f3a0a
Wan Mohd Hafiz <fizyboy@gmail.com>
2018-03-07 13:30:03 +0800

    change smart-search to smart-components

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,53 +1,56 @@
-const path = require('path')
-const camelCase = require('lodash/camelCase')
-const upperFirst = require('lodash/upperFirst')
-const { version } = require('./package.json')
+const path = require('path');
+const camelCase = require('lodash/camelCase');
+const upperFirst = require('lodash/upperFirst');
+const { version } = require('./package.json');
 
 module.exports = {
-  title: `SmartSearch | ${version}`,
+  title: `SmartComponents | ${version}`,
   template: './styleguide.template.html',
-  getComponentPathLine: (componentPath) => {
-    const dirname = path.dirname(componentPath, '.js')
-    const name = dirname.split('/').slice(-1)[0]
-    const componentName = upperFirst(camelCase(name))
+  getComponentPathLine: componentPath => {
+    const dirname = path.dirname(componentPath, '.js');
+    const name = dirname.split('/').slice(-1)[0];
+    const componentName = upperFirst(camelCase(name));
 
-    return 'import ' + componentName + ' from \'smartsearch/' + name + '\''
+    return 'import ' + componentName + " from 'smart-components/" + name + "'";
   },
   sections: [
     {
-      name: 'Common',
-      components: () => ([
+      name: 'Components',
+      components: () => [
         path.resolve(__dirname, 'source/components/button', 'index.js'),
         path.resolve(__dirname, 'source/components/button-group', 'index.js'),
         path.resolve(__dirname, 'source/components/button-social', 'index.js'),
         path.resolve(__dirname, 'source/components/icon', 'index.js'),
-      ])
+      ],
     },
     {
-      name: 'Higher Order Components',
+      name: 'Containers',
       sections: [
         {
           name: 'withToggle',
-          content: 'source/components/with-toggle/Readme.md'
-        }
-      ]
-    }
+          content: 'source/components/with-toggle/Readme.md',
+        },
+      ],
+    },
   ],
-  updateWebpackConfig: (webpackConfig) => {
+  updateWebpackConfig: webpackConfig => {
     webpackConfig.module.loaders.push(
       {
         test: /\.jsx?$/,
         include: path.join(__dirname, 'source'),
-        loader: 'babel'
-      }, {
+        loader: 'babel',
+      },
+      {
         test: /\.css$/,
         include: path.join(__dirname, 'node_modules', 'minimal.css'),
-        loader: 'style!css?modules&importLoaders=1'
+        loader: 'style!css?modules&importLoaders=1',
       }
-    )
+    );
 
-    webpackConfig.entry.push(path.join(__dirname, 'node_modules/minimal.css/minimal.css'))
+    webpackConfig.entry.push(
+      path.join(__dirname, 'node_modules/minimal.css/minimal.css')
+    );
 
-    return webpackConfig
-  }
-}
+    return webpackConfig;
+  },
+};
